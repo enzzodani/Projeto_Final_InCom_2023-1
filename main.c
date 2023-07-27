8e11dd2179bbe6783232609eeb16afb1b1ae4cf4
Itamar Rocha <itamar.itamar@discente.ufg.br>
2023-07-27 16:55:48 -0300

    fix indentation

--- main.c
+++ main.c
@@ -14,44 +14,44 @@ Josue
 
 int main()
 {
-	    char e = 'I';
-	        while(1){
-			        fflush(stdout);
-				        system("clear");
-					        switch(e){
-							         // Tela incial
-									         case 'I':
-									              // Lógica da tela inicial
-											                  printf("PROGRAMA DE VENDAS\n\n");
-										                  printf("Informe a operação desejada: \n");
-												              printf("P - Cadastro de produtos\n");
-													                  printf("F - Cadastro de formas de pagamento\n");
-															              printf("V - Processo de vendas\n");
-																                  printf("S - Sair\n");
-																		               // TODO: corrigir bug do lixo na input stream
-																					                 scanf("%c", &e);
-																				                 getchar();
-																						             break;
-																							              // Cadastro de produtos
-																									              case 'P':
-																									                   // Lógica do cadastro de produtos
-																												               e = 'I';
-																											               break;
-																												                // Cadastro de formas de pagamento
-																															        case 'F':
-																															             // Lógica do cadastro de formas de pagamento
-																																	                 e = 'I';
-																																                 break;
-																																		          // Procedimento de vendas
-																																				          case 'V':
-																																				               // Lógica do procedimento de vendas
-																																						                   e = 'I';
-																																					                   break;
-																																							            // Sai do programa
-																																									            case 'S':
-																																									                return 0;
-																																											        default:
-																																											            e = 'I';
-																																												            }
-						    }
+	char e = 'I';
+	while(1){
+		fflush(stdout);
+		system("clear");
+		switch(e){
+			// Tela incial
+			case 'I':
+				// Lógica da tela inicial
+				printf("PROGRAMA DE VENDAS\n\n");
+				printf("Informe a operação desejada: \n");
+				printf("P - Cadastro de produtos\n");
+				printf("F - Cadastro de formas de pagamento\n");
+				printf("V - Processo de vendas\n");
+				printf("S - Sair\n");
+				// TODO: corrigir bug do lixo na input stream
+				scanf("%c", &e);
+				getchar();
+				break;
+			// Cadastro de produtos
+			case 'P':
+				// Lógica do cadastro de produtos
+				e = 'I';
+				break;
+			// Cadastro de formas de pagamento
+			case 'F':
+				// Lógica do cadastro de formas de pagamento
+				e = 'I';
+				break;
+			// Procedimento de vendas
+			case 'V':
+				// Lógica do procedimento de vendas
+				e = 'I';
+				break;
+			// Sai do programa
+			case 'S':
+				return 0;
+			default:
+				e = 'I';
+		}
+	}
 }
